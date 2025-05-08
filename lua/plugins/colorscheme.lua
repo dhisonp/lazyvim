d59947fd0d567990f27adc9e32eb7bdda5d6a61f
Dhison <dhisonp@gmail.com>
2025-05-08 15:38:54 -0400

    add everforest to colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,22 @@
 return {
   { "rose-pine/neovim", name = "rose-pine" },
+  {
+    "sainnhe/everforest",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.everforest_enable_italic = true
+      vim.cmd.colorscheme("everforest")
+    end,
+  },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-macchiato",
+      colorscheme = "everforest",
     },
   },
 }

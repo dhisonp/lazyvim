6e95c3eec22d1ebf707d172c10515c242ffab6d7
Dhison <dhisonp@gmail.com>
2025-08-15 12:51:48 -0400

    use mini icons for fzf

--- lua/custom/plugins/fzf-lua.lua
+++ lua/custom/plugins/fzf-lua.lua
@@ -1,7 +1,7 @@
 return {
   'ibhagwan/fzf-lua',
   lazy = false,
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = { 'echasnovski/mini.icons' },
 
   config = function()
     -- Calling setup is optional, but good practice to have in place

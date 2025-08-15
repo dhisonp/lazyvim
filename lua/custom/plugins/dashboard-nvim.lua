44bb54616befbd72a8c3b108e826f97b6424c556
Dhison <dhisonp@gmail.com>
2025-08-15 01:22:34 -0400

    format lua and add dashboard

---
+++ lua/custom/plugins/dashboard-nvim.lua
@@ -0,0 +1,20 @@
+return {
+	'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+	'glepnir/dashboard-nvim',
+	event = 'VimEnter', -- Or 'BufWinEnter'
+	config = function()
+		require('dashboard').setup {
+			theme = 'doom',
+			disable_statusline = false,
+			disable_header = false,
+			center = {
+				{
+					icon = '󰈞  ',
+					desc = 'Find  File                              ',
+					action = 'FzfLua files',
+					key = '<Leader> f f',
+				},
+			},
+		}
+	end,
+}
--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,15 +1,15 @@
 return {
-  'akinsho/toggleterm.nvim',
-  version = '*',
-  opts = {
-    close_on_exit = true,
-    direction = 'horizontal',
-    size = 20,
-  },
-  config = function(_, opts)
-    require('toggleterm').setup(opts)
+	'akinsho/toggleterm.nvim',
+	version = '*',
+	opts = {
+		close_on_exit = true,
+		direction = 'horizontal',
+		size = 20,
+	},
+	config = function(_, opts)
+		require('toggleterm').setup(opts)
 
-    vim.keymap.set('n', '<C-j>', '<cmd>ToggleTerm<CR>', { desc = 'Toggle Terminal' })
-    vim.keymap.set('t', '<C-j>', '<cmd>ToggleTerm<CR>', { desc = 'Toggle Terminal' })
-  end,
+		vim.keymap.set('n', '<C-j>', '<cmd>ToggleTerm<CR>', { desc = 'Toggle Terminal' })
+		vim.keymap.set('t', '<C-j>', '<cmd>ToggleTerm<CR>', { desc = 'Toggle Terminal' })
+	end,
 }

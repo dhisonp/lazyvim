ae6b96f6fc3b4c7f5ddaf58df395b3752ed7393e
Dhison <dhisonp@gmail.com>
2025-08-15 00:46:16 -0400

    add kanagawa colorscheme

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -2,6 +2,7 @@ return { -- You can easily change to a different colorscheme.
   -- Change the name of the colorscheme plugin below, and then
   -- change the command in the config to whatever the name of that colorscheme is.
   'folke/tokyonight.nvim',
+  'rebelot/kanagawa.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function()
     ---@diagnostic disable-next-line: missing-fields

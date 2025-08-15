eeb85158cf791ce72951796421b96d607aace576
Dhison <dhisonp@gmail.com>
2025-08-15 13:45:17 -0400

    add copy file path keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,6 +8,12 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<leader>fc', function()
+  local relative_path = vim.fn.expand '%'
+  vim.fn.setreg('+', relative_path)
+  print(relative_path)
+end, { desc = 'Copy buffer file path' })
+
 -- fzf-lua
 vim.keymap.set('n', '<leader>ff', '<cmd>FzfLua files<CR>', { desc = 'Find files' })
 vim.keymap.set('n', '<leader>fg', '<cmd>FzfLua live_grep<CR>', { desc = 'Live grep' })

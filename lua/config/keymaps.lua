cec3ad79e4c9db63dfa750155866f0128dc0da6f
Dhison <dhisonp@gmail.com>
2025-08-15 12:49:19 -0400

    comments

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
+-- See `:help vim.keymap.set()`
+
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
@@ -32,7 +34,6 @@ vim.schedule(function()
 end)
 
 vim.o.signcolumn = 'yes'
-
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 
@@ -43,14 +44,9 @@ vim.o.splitbelow = true
 vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
--- Keymaps
--- See `:help vim.keymap.set()`
-
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic Quickfix list' })
 
--- Custom (TODO: Refactor to appropriate configs)
-vim.keymap.set('n', '<leader>uc', '<cmd>FzfLua colorschemes<CR>', { desc = 'Select Theme' })
 vim.keymap.set('n', '<leader>g', '<cmd>LazyGit<CR>', { desc = 'Open Lazygit' })
 
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
@@ -58,26 +54,26 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- Keymaps from fzf-lua.lua
+-- fzf-lua
 vim.keymap.set('n', '<leader>ff', '<cmd>FzfLua files<CR>', { desc = 'Find Files' })
 vim.keymap.set('n', '<leader>fg', '<cmd>FzfLua live_grep<CR>', { desc = 'Live Grep' })
 vim.keymap.set('n', '<leader>fb', '<cmd>FzfLua buffers<CR>', { desc = 'Find Buffers' })
 vim.keymap.set('n', '<leader>fh', '<cmd>FzfLua help_tags<CR>', { desc = 'Help Tags' })
 vim.keymap.set('n', '<leader>ft', '<cmd>FzfLua btags<CR>', { desc = 'Buffer Tags' })
-vim.keymap.set('n', '<leader>fc', '<cmd>FzfLua colorschemes<CR>', { desc = 'Color Schemes' })
+vim.keymap.set('n', '<leader>uc', '<cmd>FzfLua colorschemes<CR>', { desc = 'Select Theme' })
 vim.keymap.set('n', '<C-p>', '<cmd>FzfLua files<CR>', { desc = 'Grep Visual' })
 
--- Keymaps from grapple.lua
+-- grapple
 vim.keymap.set('n', '<leader>pt', '<cmd>Grapple toggle<cr>', { desc = 'Toggle Tag' })
 vim.keymap.set('n', '<leader>pl', '<cmd>Grapple toggle_tags<cr>', { desc = 'Tag List' })
 vim.keymap.set('n', '<leader>pj', '<cmd>Grapple cycle_tags next<cr>', { desc = 'Next Tag' })
 vim.keymap.set('n', '<leader>pk', '<cmd>Grapple cycle_tags prev<cr>', { desc = 'Prev Tag' })
 
--- Keymaps from toggleterm.lua
+-- toggleterm
 vim.keymap.set('n', '<C-j>', '<cmd>ToggleTerm<CR>', { desc = 'Toggle Terminal' })
 vim.keymap.set('t', '<C-j>', '<cmd>ToggleTerm<CR>', { desc = 'Toggle Terminal' })
 
--- Keymaps from debug.lua
+-- debug
 vim.keymap.set('n', '<F5>', '<cmd>lua require("dap").continue()<CR>', { desc = 'Debug: Start/Continue' })
 vim.keymap.set('n', '<F1>', '<cmd>lua require("dap").step_into()<CR>', { desc = 'Debug: Step Into' })
 vim.keymap.set('n', '<F2>', '<cmd>lua require("dap").step_over()<CR>', { desc = 'Debug: Step Over' })
@@ -93,7 +89,7 @@ vim.keymap.set('n', '<leader>B', function()
 end, { desc = 'Debug: Set Breakpoint' })
 vim.keymap.set('n', '<F7>', '<cmd>lua require("dapui").toggle()<CR>', { desc = 'Debug: See last session result.' })
 
--- Keymaps from conform.lua
+-- conform
 vim.keymap.set(
   '',
   '<leader>F',
@@ -101,5 +97,5 @@ vim.keymap.set(
   { desc = 'Format buffer' }
 )
 
--- Keymaps from neo-tree.lua
+-- neo-tree
 vim.keymap.set('', '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal', silent = true })

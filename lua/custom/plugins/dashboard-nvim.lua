24a7bda5f8a5f729dfbc705ceccdc036ee09abde
Dhison <dhisonp@gmail.com>
2025-08-15 01:42:03 -0400

    dashboard

--- lua/custom/plugins/dashboard-nvim.lua
+++ lua/custom/plugins/dashboard-nvim.lua
@@ -3,7 +3,33 @@ return {
   event = 'VimEnter',
   config = function()
     require('dashboard').setup {
-      -- config
+      theme = 'doom',
+      config = {
+        center = {
+          {
+            action = 'FzfLua files',
+            desc = 'Find File',
+            key = 'f',
+          },
+          {
+            action = 'Lazy',
+            desc = 'Lazy',
+            key = 'l',
+          },
+          {
+            action = 'LazyGit',
+            desc = 'LazyGit',
+            key = 'g',
+          },
+          {
+            action = function()
+              vim.api.nvim_input '<cmd>qa<cr>'
+            end,
+            desc = 'Quit',
+            key = 'q',
+          },
+        },
+      },
     }
   end,
   dependencies = { { 'nvim-tree/nvim-web-devicons' } },

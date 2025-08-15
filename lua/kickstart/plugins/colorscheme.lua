8ad514e0aa9c790e6c5c125ca686bcd650b52fd2
Dhison <dhisonp@gmail.com>
2025-08-15 00:49:33 -0400

    fix colorscheme

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,19 +1,25 @@
-return { -- You can easily change to a different colorscheme.
-  -- Change the name of the colorscheme plugin below, and then
-  -- change the command in the config to whatever the name of that colorscheme is.
-  'folke/tokyonight.nvim',
-  'rebelot/kanagawa.nvim',
-  priority = 1000, -- Make sure to load this before all the other start plugins.
-  config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require('tokyonight').setup {
-      styles = {
-        comments = { italic = false }, -- Disable italics in comments
-      },
-    }
+return {
+  -- Tokyonight colorscheme
+  {
+    'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('tokyonight').setup {
+        styles = {
+          comments = { italic = false }, -- Disable italics in comments
+        },
+      }
+    end,
+  },
 
-    -- Load the colorscheme here.
-    vim.cmd.colorscheme 'kanagawa'
-  end,
+  -- Kanagawa colorscheme
+  {
+    'rebelot/kanagawa.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      -- Load the colorscheme here
+      vim.cmd.colorscheme 'kanagawa'
+    end,
+  },
 }
-

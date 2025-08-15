82657ae2a8d9d345634928f70313f36b588486b7
Dhison <dhisonp@gmail.com>
2025-08-15 12:57:25 -0400

    refactor plugins into single directory

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,25 +19,7 @@ local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
 require('lazy').setup({
-  require 'kickstart.plugins.guess_indent',
-  require 'kickstart.plugins.gitsigns_base',
-  require 'kickstart.plugins.which_key',
-  require 'kickstart.plugins.lazydev',
-  require 'kickstart.plugins.mason',
-  require 'kickstart.plugins.conform',
-  require 'kickstart.plugins.blink_cmp',
-  require 'kickstart.plugins.colorscheme',
-  require 'kickstart.plugins.todo_comments',
-  require 'kickstart.plugins.mini',
-  require 'kickstart.plugins.treesitter',
-  require 'kickstart.plugins.debug',
-  require 'kickstart.plugins.indent_blankline',
-  require 'kickstart.plugins.lint',
-  require 'kickstart.plugins.autopairs',
-  require 'kickstart.plugins.neo-tree',
-  require 'kickstart.plugins.gitsigns',
-
-  { import = 'custom.plugins' },
+  { import = 'plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the

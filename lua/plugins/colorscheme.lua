88567c0775120ef623d4d42b916a9809dfbe72cb
Dhison <dhisonp@gmail.com>
2025-08-19 14:53:41 -0400

    adjust indent line character

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,6 +2,7 @@ return {
   {
     'folke/tokyonight.nvim',
     priority = 1000,
+    lazy = true,
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup({
--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -22,6 +22,9 @@ return {
       scope = {
         enabled = false,
       },
+      indent = {
+        char = '╎',
+      },
     },
   },
 }

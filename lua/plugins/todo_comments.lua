ad039681a5100bcccba277afa3434aff0c505bd7
Dhison <dhisonp@gmail.com>
2025-08-19 14:43:31 -0400

    do not highlight TODO/NOTEs

--- lua/plugins/todo_comments.lua
+++ lua/plugins/todo_comments.lua
@@ -2,5 +2,12 @@ return { -- Highlight todo, notes, etc in comments
   'folke/todo-comments.nvim',
   event = 'VimEnter',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = { signs = false },
+  opts = {
+    signs = false,
+    highlight = {
+      before = "",
+      keyword = "fg",
+      after = "fg",
+    },
+  },
 }

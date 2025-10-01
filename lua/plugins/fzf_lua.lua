675d7b7ad62d9eebf2e3896d8818d96cc6b4f53c
Dhison <dhisonp@gmail.com>
2025-10-01 18:09:36 -0400

    minor fzf-lua optimizations

--- lua/plugins/fzf_lua.lua
+++ lua/plugins/fzf_lua.lua
@@ -4,8 +4,27 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
 
   config = function()
-    require('fzf-lua').setup({})
-    -- Register as vim.ui.select backend to fix code actions
+    require('fzf-lua').setup({
+      winopts = {
+        height = 0.85,
+        width = 0.80,
+        preview = {
+          scrollbar = false,
+        },
+      },
+      files = {
+        cmd = 'fd --type f --hidden --follow --exclude .git',
+        fzf_opts = { ['--algo'] = 'v2' },
+      },
+      grep = {
+        cmd = 'rg --column --line-number --no-heading --color=always --smart-case --max-columns=4096',
+        fzf_opts = { ['--algo'] = 'v2' },
+      },
+      fzf_opts = {
+        ['--algo'] = 'v2',
+        ['--no-scrollbar'] = true,
+      },
+    })
     require('fzf-lua').register_ui_select()
   end,
 }

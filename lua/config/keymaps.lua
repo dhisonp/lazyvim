8ddbe655a74d5ccc19aaaab5757c253418851174
Dhison <dhisonp@gmail.com>
2025-10-10 13:03:30 -0400

    fix K show docs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -26,6 +26,17 @@ vim.keymap.set('n', '<C-c>', 'gcc', { remap = true })
 vim.keymap.set('v', '<C-c>', 'gc', { remap = true })
 
 -- COC
+function _G.show_docs()
+  local cw = vim.fn.expand('<cword>')
+  if vim.fn.index({ 'vim', 'help' }, vim.bo.filetype) >= 0 then
+    vim.api.nvim_command('h ' .. cw)
+  elseif vim.api.nvim_eval('coc#rpc#ready()') then
+    vim.fn.CocActionAsync('doHover')
+  else
+    vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+  end
+end
+vim.keymap.set('n', 'K', '<CMD>lua _G.show_docs()<CR>', { desc = 'Show documentation', silent = true })
 vim.keymap.set('n', '<leader>r', '<Plug>(coc-rename)', { desc = 'Rename symbol' })
 vim.keymap.set(
   'i',

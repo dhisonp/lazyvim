0cdd0de272871fc8d929770187f49481bd84b58a
Dhison <dhisonp@gmail.com>
2026-05-07 13:23:06 -0400

    lazy lock

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
   branch = 'main',
+  build = ':TSUpdate',
   event = { 'BufReadPost', 'BufNewFile' },
   opts = {
     -- A list of parser names, or "all"

dcd604890c21bb6b7e4c895f8e98ac240dba6fe2
Dhison <dhisonp@gmail.com>
2026-05-18 11:38:34 -0400

    Simplify mason.lua

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -12,92 +12,51 @@ return {
     -- Useful status updates for LSP.
     { 'j-hui/fidget.nvim', opts = {} },
 
-    -- Allows extra capabilities provided by blink.cmp
+    -- Ensure blink.cmp is loaded before LSP attaches so it can register its capabilities
     'saghen/blink.cmp',
   },
   config = function()
-    -- Reduce LSP log verbosity to prevent log bloat (Neovim 0.11+)
-    if vim.lsp.log and vim.lsp.log.set_level then
-      vim.lsp.log.set_level(vim.log.levels.ERROR)
-    end
+    vim.lsp.log.set_level(vim.log.levels.ERROR)
 
     vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+      group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
       callback = function(event)
-        -- for LSP related items. It sets the mode, buffer and description for us each time.
-        local map = function(keys, func, desc, mode)
-          mode = mode or 'n'
-          vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-        end
-
-        -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
-        ---@param client vim.lsp.Client
-        ---@param method vim.lsp.protocol.Method
-        ---@param bufnr? integer some lsp support methods only in specific files
-        ---@return boolean
-        local function client_supports_method(client, method, bufnr)
-          if vim.fn.has 'nvim-0.11' == 1 then
-            return client:supports_method(method, bufnr)
-          else
-            return client.supports_method(method, { bufnr = bufnr })
-          end
-        end
-
-        -- The following two autocommands are used to highlight references of the
-        -- word under your cursor when your cursor rests there for a little while.
-        --    See `:help CursorHold` for information about when this is executed
-        --
-        -- When you move your cursor, the highlights will be cleared (the second autocommand).
         local client = vim.lsp.get_client_by_id(event.data.client_id)
+
         if
           client
-          and client_supports_method(
-            client,
+          and client:supports_method(
             vim.lsp.protocol.Methods.textDocument_documentHighlight,
             event.buf
           )
         then
-          local highlight_augroup =
-            vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+          local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
           vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
             buffer = event.buf,
             group = highlight_augroup,
             callback = vim.lsp.buf.document_highlight,
           })
-
           vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
             buffer = event.buf,
             group = highlight_augroup,
             callback = vim.lsp.buf.clear_references,
           })
-
           vim.api.nvim_create_autocmd('LspDetach', {
-            group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+            group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
             callback = function(event2)
               vim.lsp.buf.clear_references()
-              vim.api.nvim_clear_autocmds({
-                group = 'kickstart-lsp-highlight',
-                buffer = event2.buf,
-              })
+              vim.api.nvim_clear_autocmds({ group = 'lsp-highlight', buffer = event2.buf })
             end,
           })
         end
 
-        -- The following code creates a keymap to toggle inlay hints in your
-        -- code, if the language server you are using supports them
-        --
-        -- This may be unwanted, since they displace some of your code
         if
           client
-          and client_supports_method(
-            client,
-            vim.lsp.protocol.Methods.textDocument_inlayHint,
-            event.buf
-          )
+          and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf)
         then
-          map('<leader>uh', function()
+          vim.keymap.set('n', '<leader>uh', function()
             vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
-          end, 'Toggle Inlay Hints')
+          end, { buffer = event.buf, desc = 'LSP: Toggle Inlay Hints' })
         end
       end,
     })
@@ -119,45 +78,32 @@ return {
       virtual_text = false,
     })
 
-    -- LSP servers and clients are able to communicate to each other what features they support.
-    --  By default, Neovim doesn't support everything that is in the LSP specification.
-    --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
-    --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
-    local capabilities = require('blink.cmp').get_lsp_capabilities()
-
-    local servers = {
-      lua_ls = {
-        settings = {
-          Lua = {
-            completion = {
-              callSnippet = 'Replace',
-            },
-          },
-        },
-      },
-    }
-
-    local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      'stylua', -- Used to format Lua code
-      'markdownlint', -- Used to lint markdown files
-    })
     require('mason-tool-installer').setup({
-      ensure_installed = ensure_installed,
+      ensure_installed = {
+        'lua_ls',
+        'stylua',
+        'markdownlint',
+      },
     })
 
     require('mason-lspconfig').setup({
-      ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-      automatic_installation = false,
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          server.capabilities =
-            vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          require('lspconfig')[server_name].setup(server)
-        end,
+      -- Auto-enable all installed servers except those managed explicitly via vim.lsp.enable below
+      automatic_enable = {
+        exclude = { 'basedpyright', 'lua_ls', 'vtsls', 'vue_ls' },
       },
     })
+
+    -- Lua
+    vim.lsp.config('lua_ls', {
+      settings = {
+        Lua = {
+          completion = {
+            callSnippet = 'Replace',
+          },
+        },
+      },
+    })
+    vim.lsp.enable 'lua_ls'
 
     -- Ty
     vim.lsp.config('ty', {
@@ -173,8 +119,9 @@ return {
     vim.lsp.enable 'ty'
 
     -- Vue - use Mason-managed language server
-    local mason_packages = vim.fn.stdpath('data') .. '/mason/packages'
-    local vue_language_server_path = mason_packages .. '/vue-language-server/node_modules/@vue/language-server'
+    local mason_packages = vim.fn.stdpath 'data' .. '/mason/packages'
+    local vue_language_server_path = mason_packages
+      .. '/vue-language-server/node_modules/@vue/language-server'
     local tsserver_filetypes =
       { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
     local vue_plugin = {

2127fe2d4bcd90a7b2cced82655e59dd6f4d82c3
Dhison <dhisonp@gmail.com>
2026-09-03 12:29:47 -0400

    Restore window navigation keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -52,3 +52,8 @@ end, { desc = 'Diff buffer' })
 vim.keymap.set({ 'o', 'x' }, 'ih', function()
   require('gitsigns').select_hunk()
 end, { desc = 'Hunk' })
+
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
